30b5a5544ae1578ddd951985c300e44551c15b7b
Arsalan Ahmed <arsa666.k@gmail.com>
2014-06-14 22:12:53 -0500

    radio check server before running and enabled disabled buttons

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ var namaz = require("namazTab");
 var jamat = require("jamatTab");
 var radio = require("radioTab");
 var tabGroup = Titanium.UI.createTabGroup();
+tabGroup.addTab(radio.getRadioTab);
 tabGroup.addTab(namaz.getNamazTab);
 tabGroup.addTab(jamat.getjamatTab);
-tabGroup.addTab(radio.getRadioTab);
-
 tabGroup.open();
+Ti.UI.setBackgroundImage( '/images/background.jpg' );
