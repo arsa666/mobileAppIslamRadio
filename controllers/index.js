92afe87f4ee3ca66880be89b5b369d95b8f029f4
Arsalan Ahmed <arsa666.k@gmail.com>
2014-10-12 11:13:43 -0500

    lala

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,22 @@
-//var namaz = require("namazTab");
-var jamat = require("jamatTab");
-var radio = require("radioTab");
-var colon = require("colonTab");
+// //var namaz = require("namazTab");
+// var jamat = require("jamatTab");
+// var radio = require("radioTab");
+// var colon = require("colonTab");
 
 
 
-var tabGroup = Titanium.UI.createTabGroup();
-tabGroup.addTab(radio.getRadioTab);
-tabGroup.addTab(colon.getRadioTab);
-//tabGroup.addTab(namaz.getNamazTab);
-tabGroup.addTab(jamat.getjamatTab);
+// var tabGroup = Titanium.UI.createTabGroup();
+// tabGroup.addTab(radio.getRadioTab);
+// tabGroup.addTab(colon.getRadioTab);
+// //tabGroup.addTab(namaz.getNamazTab);
+// tabGroup.addTab(jamat.getjamatTab);
 
-tabGroup.open();
-Ti.UI.setBackgroundImage( '/images/background.png' );
+// tabGroup.open();
+// Ti.UI.setBackgroundImage( '/images/background.png' );
+
+$.index.open();
+
+
+function searchSalat(e){
+    Titanium.API.info("You clicked the button");
+};
