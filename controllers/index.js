509a6098f9d2e593f98b58b05cef6781a814ee4a
Arsalan Ahmed <arsa666.k@gmail.com>
2014-12-16 20:26:56 -0500

    final version before going to pakistan Alhamdulillah

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,197 @@
+var Cloud = require("ti.cloud");
+var jamaToken = null;
+var colonToken = null;
+
+if (Ti.Platform.osname === 'iphone' || Ti.Platform.osname === 'ipad')
+var service = Ti.App.iOS.registerBackgroundService({url:'bg-service1.js'});
+
 //variables
-var audioPlayerJama = Ti.Media.createAudioPlayer({
-    url: 'http://107.170.87.104:8000/stream',
+Ti.App.audioPlayer = Ti.Media.createAudioPlayer({
     allowBackground: true
 });
+
+
+function subscribeJama () {
+    if (jamaToken === null){
+        if (Ti.Platform.name == 'android') {
+        var CloudPush = require('ti.cloudpush');
+         // These events monitor incoming push notifications
+            CloudPush.retrieveDeviceToken({
+                success : function deviceTokenSuccess(e) {
+                    subscribeToChannelJama(e);
+                },
+                error : function deviceTokenError(e) {
+                    subscribeError(e);
+                }
+            });
+            CloudPush.addEventListener('callback', function (evt) {
+                var and = JSON.parse(evt.payload);
+                //alert(and['android']['alert']);
+            });
+            CloudPush.showTrayNotificationsWhenFocused = true;
+
+        } else {
+            Ti.Network.registerForPushNotifications({
+                // Specifies which notifications to receive
+                types: [
+                    Ti.Network.NOTIFICATION_TYPE_BADGE,
+                    Ti.Network.NOTIFICATION_TYPE_ALERT,
+                    Ti.Network.NOTIFICATION_TYPE_SOUND
+                ],
+                success: function deviceTokenSuccess(e) {
+                    subscribeToChannelJama(e);
+                },
+                error: function deviceTokenError(e) {
+                    subscribeError(e);
+                }
+            });
+        }
+    }
+};
+
+function subscribeColon () {
+    if (colonToken === null){
+        if (Ti.Platform.name == 'android') {
+        var CloudPush = require('ti.cloudpush');
+         // These events monitor incoming push notifications
+            CloudPush.retrieveDeviceToken({
+                success : function deviceTokenSuccess(e) {
+                    subscribeToChannelColon(e);
+                },
+                error : function deviceTokenError(e) {
+                    subscribeError(e);
+                }
+            });
+            CloudPush.addEventListener('callback', function (evt) {
+                var and = JSON.parse(evt.payload);
+                //alert(and['android']['alert']);
+            });
+            CloudPush.showTrayNotificationsWhenFocused = true;
+
+        } else {
+            Ti.Network.registerForPushNotifications({
+                // Specifies which notifications to receive
+                types: [
+                    Ti.Network.NOTIFICATION_TYPE_BADGE,
+                    Ti.Network.NOTIFICATION_TYPE_ALERT,
+                    Ti.Network.NOTIFICATION_TYPE_SOUND
+                ],
+                success: function deviceTokenSuccess(e) {
+                    subscribeToChannelColon(e);
+                },
+                error: function deviceTokenError(e) {
+                    subscribeError(e);
+                }
+            });
+        }
+    }
+};
+
+function subscribeError(e) {
+    Ti.API.info('Error al registrar token: ' + e.deviceToken);
+};
+
+
+function subscribeToChannelJama (e) {
+    jamaToken = e.deviceToken;
+    Cloud.PushNotifications.subscribeToken({
+        device_token: jamaToken,
+        channel: 'jama_prod',
+        type: Ti.Platform.name == 'android' ? 'android' : 'ios'
+    }, function (e) {
+        if (e.success) {
+            alert('Se ha registrado correctamente, ahora recibira las notificaciones para esta radio.')
+            Ti.API.info('Device Token subscribed: ' + e.deviceToken);
+        } else {
+            alert('Hubo un error en su registracion, porfavor conectese al internet e intente denuevo.')
+            Ti.API.info('Device Token error subscrbie: ' + e.deviceToken);
+        }
+    });
+};
+
+function subscribeToChannelColon (e) {
+    colonToken = e.deviceToken;
+    Cloud.PushNotifications.subscribeToken({
+        device_token: colonToken,
+        channel: 'colon_prod',
+        type: Ti.Platform.name == 'android' ? 'android' : 'ios'
+    }, function (e) {
+        if (e.success) {
+            alert('Se ha registrado correctamente, ahora recibira las notificaciones para esta radio.')
+            Ti.API.info('Device Token subscribed: ' + e.deviceToken);
+        } else {
+            alert('Hubo un error en su registracion, porfavor conectese al internet e intente denuevo.')
+            Ti.API.info('Device Token error subscrbie: ' + e.deviceToken);
+        }
+    });
+};
+
+function unSubscribeColon() {
+    Cloud.PushNotifications.unsubscribeToken({
+    channel: 'colon_prod',
+    device_token: colonToken
+    }, function (e) {
+        if (e.success) {
+            alert('Ya no recibira push notificaciones de Radio Colon.');
+            colonToken = null;
+        } else {
+            Ti.API.info('Error:\n' +
+                ((e.error && e.message) || JSON.stringify(e)));
+        }
+    });
+};
+
+function unSubscribeJama() {
+    Cloud.PushNotifications.unsubscribeToken({
+    channel: 'jama_prod',
+    device_token: jamaToken
+    }, function (e) {
+        if (e.success) {
+            alert('Ya no recibira push notificaciones de radio Jama.');
+            jamaToken = null;
+        } else {
+            Ti.API.info('Error:\n' +
+                ((e.error && e.message) || JSON.stringify(e)));
+        }
+    });
+};
+
+
+
+$.setSwitchState.addEventListener('focus', function (e) {
+   var jamaNotifications =  Ti.App.Properties.getBool('switchJamaNotifications');
+   var colonNotifications =  Ti.App.Properties.getBool('switchColonNotifications');
+
+   if (jamaNotifications !== null) {
+        $.jamaSwitch.value = jamaNotifications;
+   }
+   if (colonNotifications !== null) {
+        $.colonSwitch.value = colonNotifications;
+   }
+});
+
+function switchJamaNotifications(e) {
+    var val = e.value;
+    if (val === true && Ti.App.Properties.getBool('switchJamaNotifications') === false) {
+        Ti.App.Properties.setBool('switchJamaNotifications', true);
+        subscribeJama();
+    } else {
+        Ti.App.Properties.setBool('switchJamaNotifications', false);
+        unSubscribeJama();
+    }
+};
+
+function switchColonNotifications(e) {
+    var val = e.value;
+    if (val === true && Ti.App.Properties.getBool('switchColonNotifications') === false) {
+        Ti.App.Properties.setBool('switchColonNotifications', true);
+        subscribeColon();
+    } else {
+        Ti.App.Properties.setBool('switchColonNotifications', false);
+        unSubscribeColon();
+    }
+};
+
 
 //disabled the btn passed in
 function makeDisabled (btn) {
@@ -20,6 +209,11 @@ function makeEnabled(btn){
     btn.color ='#0066FF';
 
 }
+
+//open website
+function openWeb() {
+    Ti.Platform.openURL("http://www.islamenpanama.com");
+};
 
 //get salat and jamat times as json REST API
 function searchSalat(e){
@@ -49,13 +243,13 @@ function searchSalat(e){
         $.salatAsar.text = 'Asar: ' + response['asar'] + ' P.M';
         $.salatMagrib.text = 'Magrib: ' + response['magrib'] + ' P.M';
         $.salatIsha.text = 'Isha: ' + response['isha'] + ' P.M';
-    }
+    };
 };
 
 
 //turn jama radio on
 function jamaRadioOn(e) {
-
+    stopRadio();
     $.radioStatus.text = 'Loading...';
     makeDisabled($.jamaRadioOn);
     makeEnabled($.jamaRadioOff);
@@ -80,7 +274,9 @@ function jamaRadioOn(e) {
         var response = this.responseText.indexOf('class="streamdata"');
         if (response !== -1) {
             Ti.API.info('SERVER ON');
-            audioPlayerJama.start();
+            Ti.App.audioPlayer.url = 'http://107.170.87.104:8000/stream';
+            Titanium.Media.audioSessionMode = Ti.Media.AUDIO_SESSION_MODE_PLAYBACK;
+            Ti.App.audioPlayer.start();
             makeDisabled($.jamaRadioOn);
             makeEnabled($.jamaRadioOff);
             $.radioStatus.text = 'En Vivo...';
@@ -93,19 +289,121 @@ function jamaRadioOn(e) {
     };
 };
 
+//turn colon radio on
+function colonRadioOn(e) {
+    stopRadio();
+    $.radioStatusColon.text = 'Loading...';
+    makeDisabled($.colonRadioOn);
+    makeDisabled($.colonRadioArabeOn);
+    makeEnabled($.colonRadioOff);
+    var xhr = Ti.Network.createHTTPClient({cache:false});
+    xhr.open('GET', 'http://162.243.80.141:8000/status.xsl');
+    xhr.send();
+
+
+    xhr.onerror = function(e) {
+        Ti.API.info('ON ERROR');
+
+        Ti.API.info(xhr.status);
+        makeDisabled($.colonRadioOff);
+        makeEnabled($.colonRadioOn);
+        makeEnabled($.colonRadioArabeOn);
+
+
+        Ti.API.error('Bad Server =>' + e.error);
+        $.radioStatusColon.text = 'Radio Offline!';
+        alert('Asegurese de estar conectado a internet.');
+    };
+
+    xhr.onload = function(){
+        var response = this.responseText.indexOf('class="streamdata"');
+        if (response !== -1) {
+            Ti.API.info('SERVER ON');
+            Ti.App.audioPlayer.url = 'http://162.243.80.141:8000/stream';
+            Ti.App.audioPlayer.start();
+            makeDisabled($.colonRadioOn);
+            makeDisabled($.colonRadioArabeOn);
+            makeEnabled($.colonRadioOff);
+            $.radioStatusColon.text = 'En Vivo...';
+        } else {
+            alert('Radio colon español no esta en vivo en este momento, intente a otras horas.');
+            $.radioStatusColon.text = 'Apagado';
+            makeDisabled($.colonRadioOff);
+            makeEnabled($.colonRadioArabeOn);
+            makeEnabled($.colonRadioOn);
+        }
+    };
+};
+
+//turn colon radio on
+function colonRadioArabeOn(e) {
+    stopRadio();
+    $.radioStatusColon.text = 'Loading...';
+    makeDisabled($.colonRadioOn);
+    makeDisabled($.colonRadioArabeOn);
+    makeEnabled($.colonRadioOff);
+    var xhr = Ti.Network.createHTTPClient({cache:false});
+    xhr.open('GET', 'http://107.170.156.66:8000/status.xsl');
+    xhr.send();
+
+
+    xhr.onerror = function(e) {
+        Ti.API.info('ON ERROR');
+
+        Ti.API.info(xhr.status);
+        makeDisabled($.colonRadioOff);
+        makeEnabled($.colonRadioOn);
+
+        Ti.API.error('Bad Server =>' + e.error);
+        $.radioStatusColon.text = 'Radio Offline!';
+        alert('Asegurese de estar conectado a internet.');
+    };
+
+    xhr.onload = function(){
+        var response = this.responseText.indexOf('class="streamdata"');
+        if (response !== -1) {
+            Ti.API.info('SERVER ON');
+            Ti.App.audioPlayer.url = 'http://107.170.156.66:8000/stream';
+            Ti.App.audioPlayer.start();
+            makeDisabled($.colonRadioOn);
+            makeEnabled($.colonRadioOff);
+            $.radioStatusColon.text = 'En Vivo...';
+        } else {
+            alert('Radio colon arabe no esta en vivo en este momento, intente a otras horas.');
+            $.radioStatusColon.text = 'Apagado';
+            makeDisabled($.colonRadioOff);
+            makeEnabled($.colonRadioOn);
+            makeEnabled($.colonRadioArabeOn);
+
+        }
+    };
+};
+
+
+
+function stopRadio(e){
+    Ti.App.audioPlayer.stop();
+    if (Ti.Platform.osname === 'android')
+    {
+        Ti.App.audioPlayer.release();
+    }
+};
 
 function jamaRadioOff(e) {
     Ti.API.info('STOP RADIO FUNCTION');
-    audioPlayerJama.stop();
+    stopRadio(e);
     makeEnabled($.jamaRadioOn);
     makeDisabled($.jamaRadioOff);
-    if (Ti.Platform.osname === 'android')
-    {
-        audioPlayerJama.release();
-    }
-    alert('Radio Apagado');
     $.radioStatus.text = 'Apagado';
 };
 
+function colonRadioOff(e) {
+    Ti.API.info('STOP RADIO FUNCTION');
+    stopRadio(e);
+    makeEnabled($.colonRadioOn);
+    makeEnabled($.colonRadioArabeOn);
+    makeDisabled($.colonRadioOff);
+    $.radioStatusColon.text = 'Apagado';
+};
 
 $.index.open();
